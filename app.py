e14c139a60cb16b030b03b6dc5ce300b155fb85b
Jenna Silvestre <silvestrejennamae7@gmail.com>
2025-12-11 03:11:08 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    port = int(os.getenv('PORT', 5055))
+    set
+    app.run(debug=True, port=port)
